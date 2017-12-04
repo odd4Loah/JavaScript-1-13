1aa80b87e11831c6788a2255c77c419577f8ba55
li hao <whalesong939@163.com>
2017-12-04 21:10:02 +0800

    20171204-进行js-task3-页面跳转完成，js实现

---
+++ task2-4/js/judgeLog.js
@@ -0,0 +1,10 @@
+/**
+ * Created by odd-hoo on 2017/12/04.
+ */
+
+//获取上一个页面储存的数据并用到这个页面
+var playBoy = JSON.parse(sessionStorage.getItem('playerRoleTrans'));
+var playNum = window.sessionStorage.getItem('playBar');
+console.log(playBoy);
+console.log(playNum);
+

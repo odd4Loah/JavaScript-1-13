978a8ef46ca2e65c380e7894977901a9c17ef10f
li hao <whalesong939@163.com>
2017-12-05 14:26:10 +0800

    20171205-进行js-task3-页面跳转完成，js实现,修改css样式

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,62 @@
 
 
-var message;
-alert();
-alert(age);
+// var message;
+// alert();
+// alert(age);
+//
+// var car = null;
+// console.log(typeof car);
 
-var car = null;
-console.log(typeof car);
 
+
+
+// Array.prototype.shuffle = function() {
+//     var input = this;
+//     for (var i = input.length - 1; i >= 0; i--) {
+//                         //6
+//         var j = Math.floor(Math.random() * (i + 1));
+//                          //向下取整     //取[0,1)的数字
+//                                         //0.7       //5 + 1  //0.7 * 6 = 4.2  //4
+//         //获取小于this.length的随机整数
+//         var faker = input[j];        //4
+//         input[j] = input[i];         //5
+//         input[i] = faker;            //input[j]和input[i]交换值
+//     }
+//     return input;
+// }
+
+
+// var butterFaker = [0,1,2,3,4,5,6,7];
+//
+// function shuffle(array) {
+//     var gangster = array.concat();
+//     for (var i = butterFaker.length; i--; ) {
+//         var j = Math.floor(Math.random() * (i + 1));       //值 = Math.floor(Math.random() * 可能值的总数 + 第一个可能的值)
+//         var temp = gangster[i];
+//         gangster[i] = gangster[j];
+//         gangster[j] = temp;
+//     }
+// // console.log(gangster);
+//     return gangster;
+// }
+
+
+if (!Array.prototype.shuffle) {
+    Array.prototype.shuffle = function() {
+        for (var j, x, i = this.length; i; j = parseInt(Math.random() * i) {
+            //解析一个字符串，并返回一个整数
+            x = this[--i], this[i] = this[j], this[j] = x);
+            return this;
+        }
+    };
+}
+
+var arr = [1,2,3,4,5,6,7,8,9,10];
+var x = 0;
+for(var i = 0; i < 100000; i++) {
+    shuffle(arr);
+    if( arr[0] === 1 ) {
+        x++;
+    }
+}
+console.log(x);

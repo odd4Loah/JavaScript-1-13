f63f1f05d1ac281610534189d3bcaea8a8a75d78
li hao <whalesong939@163.com>
2017-12-16 20:20:20 +0800

    20171216-进行js-task5-获取input输入内容，改写样式

---
+++ task5-10/js/backLogin.js
@@ -0,0 +1,12 @@
+/**
+ * Created by odd-hoo on 2017/12/16.
+ */
+
+$(document).ready(function () {
+    $('#sign-in').click(function () {
+        var ss = $('#result1').val();
+        var zz = $('#result2').val();
+        console.log(ss);
+        console.log(zz);
+    });
+});
